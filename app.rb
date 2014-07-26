b0bb8a94a8d3981781677153a9798752b1eb6315
James Darling <james@abscond.org>
2014-07-26 20:21:00 +0100

    fix dbconfig for heroku (hopfeully)

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require './config/environments'
 require './models/link'
 require './lib/tinyletter_scraper'
 
-dbconfig = YAML.load(File.read("config/database.yml"))
+dbconfig = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)
 RACK_ENV ||= ENV["RACK_ENV"] || "development"
 ActiveRecord::Base.establish_connection dbconfig[RACK_ENV]
 
